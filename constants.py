b2008bf1ace007be69015b0f0c7a9cd2b0ba35c1
Alejandr0x <alejandrosarmientoacosta@gmail.com>
2025-04-28 17:42:53 -0500

    Preprocesado

--- constants.py
+++ constants.py
@@ -1,5 +1,4 @@
-import os
-
+from utils import path_concat
 #Debido al gran tamaño de los datos tomaré solo tomaré las reviews de "Videogames", "Electronica" y "Teléfono y accesorios" para luego realizar un muestreo 
 DATASETS_URL = ['https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/raw/review_categories/Video_Games.jsonl.gz', 'https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/raw/review_categories/Electronics.jsonl.gz', 'https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/raw/review_categories/Cell_Phones_and_Accessories.jsonl.gz']
 
@@ -7,7 +6,13 @@ DATASETS_URL = ['https://mcauleylab.ucsd.edu/public_datasets/data/amazon_2023/ra
 DATASETS_FOLDER_PATH = './Datasets/'
 
 # Unimos la carpeta de destino con el nombre de cada archivo
-DATASETS_PATH = [os.path.join(DATASETS_FOLDER_PATH, url.split('/')[-1][:-3]) for url in DATASETS_URL]
+DATASETS_PATH = [path_concat(DATASETS_FOLDER_PATH, url.split('/')[-1][:-3]) for url in DATASETS_URL]
 
 # Ruta del archivo para el muestreo
 MUESTRA_DS_PATH = './Datasets/muestra.jsonl'
+
+# Ruta del dataframe final del EDA con sentimientos en OneHot
+CODIFIED_DF_PATH = path_concat(DATASETS_FOLDER_PATH, 'codified_df.csv')
+
+# Ruta del dataframe final del procesamiento con procesado
+PROCESSED_DF_PATH = path_concat(DATASETS_FOLDER_PATH, 'processed_df.csv')
--- utils.py
+++ utils.py
@@ -1,22 +1,60 @@
 import re
 import spacy 
+import nltk
 from nltk.corpus import stopwords
+import os
 
+# Descargamos los stopwords de nltk
+nltk.download('stopwords')
+
+# Cargamos el modelo de Spacy
 lemmatizer = spacy.load('en_core_web_sm')
 
-stopwords = set(stopwords.words('english'))
+    # Guardo las stopwords en un set
+stop_words = set(stopwords.words('english'))
 
 def limpiar_texto(text):
+    
+    """
+    Recibe un texto y lo procesa (Minúsculas, eliminación de caracteres innecesarios y doles espacios, eliminación de stopwords y lematización.)
+    Args:
+        text(string): Texto crudo
+    Returns:
+        text(string): Devuelve el texto procesado
+    """
+    
+    # Pasamos los caracteres a minúsculas
     text = text.lower()
     
-    text = re.sub(r'[^a-s\s]', '', text)
+    # Eliminamos caracteres no alfabéticos
+    text = re.sub(r'[^a-z\s]', '', text)
     
+    # Eliminamos multiples espacios
     text = re.sub(r'\s+', ' ', text).strip()
     
-    text = ' '.join([word for word in text.split() if word not in stopwords])
+    # Eliminamos las stopwords
+    text = ' '.join([word for word in text.split() if word not in stop_words])
     
+    # Usamos spicy para lematizar el texto
     lemma = lemmatizer(text)
     
-    text = ' '.join([word.lemma_ for word in lemma])
+    # Juntamos los lemas de las palabras en un solo texto
+    text = ' '.join([token.lemma_ for token in lemma])
     
     return text
+
+# Para evitar redundancia al importar os en todas los notebooks mejor creo una función aquí y será aquí el único lugar en el que la importe
+#Función para saber si una ruta ya existe
+def path_exists(path):
+    isExisting = os.path.exists(path)
+    
+    return isExisting
+
+#Función para unir dos rutas
+def path_concat(pathOne, pathTwo):
+    final_path = os.path.join(pathOne, pathTwo)
+    return final_path
+
+# Función para crear un directorio
+def make_dir(path, exist_ok):
+    os.makedirs(path, exist_ok=exist_ok)
